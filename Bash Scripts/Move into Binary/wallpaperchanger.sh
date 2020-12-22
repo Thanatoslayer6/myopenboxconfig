93880cbce89b0a2b97df50cd896e4101dcbd5ca6
Thanatoslayer6 <thanatoslayer6@gmail.com>
2020-12-22 16:13:06 +0800

    Fixed some stuff for cleaner purposes

--- Bash Scripts/Move into Binary/wallpaperchanger.sh
+++ Bash Scripts/Move into Binary/wallpaperchanger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Some kind of wallpaper changer for nitrogen I guess, this changes nitrogen for #only random wallpapers included in your directory.
-
-#sleep for 10 seconds to restore the current wallpaper.....
+#Paste this in your '/usr/bin' to make this script accessible in your startup
+#sleep for 10 seconds to restore the current wallpaper..... change it if you want
 #sleep 20
 
 COUNT=10
@@ -9,8 +9,8 @@ COUNT=10
 while [[ $COUNT == 0 ]]
 do
 	((COUNT--))
-	echo -n $COUNT 
 	sleep 1
 done
-	nitrogen --random --set-auto
+	echo "Changing Wallpaper"
+	nitrogen --random --set-scaled
 	wallpaperchanger.sh
